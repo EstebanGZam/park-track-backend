18389f69f3497b4bafebdacded9267d8f17d10cf
EstebanGZam <estebangaviria82@gmail.com>
2024-09-20 17:54:49 -0500

    initial commit

---
+++ src/main/java/com/park_track_backend/ParkTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.park_track_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkTrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/park_track_backend/ParkTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park_track_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
